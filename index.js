9bdb355c1b2fda9e78b3d76f0b4eacf2012981ad
tranceyos2419 <tranceyos2419@gmail.com>
2019-04-06 22:18:02 +0900

    static website

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-// middleware can add something to req object in express
-// middlewares are functions which can access to request and response object
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//* initialize middleware
+// set static website
+//! if you want to make a dynamic website, you use template engines or front-end framework
+app.use(express.static(path.join(__dirname, "public")));
+app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("hello");
