c08c1c3dcdc8a88ec3366820ecb5bf01aa6f3c6f
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2019-04-29 19:37:23 +0900

    first commit

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 // set static website
 //! if you want to make a dynamic website, you use template engines or front-end framework
 // app.use(express.static(path.join(__dirname, "public")));
+
+// view engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
